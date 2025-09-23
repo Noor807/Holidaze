d7bb898b489e56c0438017328e307cb2fdebe39a
Noor Irfan <152153984+Noor807@users.noreply.github.com>
2025-09-23 22:37:28 +0200

    add proper error handling

--- holidaze-app/src/utils/axios.ts
+++ holidaze-app/src/utils/axios.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+/* error handling or auth tokens*/
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API error:", error);
+    return Promise.reject(error);
+  }
+);
